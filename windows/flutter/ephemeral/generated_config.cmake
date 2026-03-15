b6f049876024f6e7b0d858a8557723aeea4c99aa
Bright-ch23 <chehermannbright@gmail.com>
2026-03-15 19:08:55 +0100

    the code for the student grade calculator in the dart programming language

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.38.6-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\HP\\AndroidStudioProjects\\grade_calculator" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.38.6-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\HP\\AndroidStudioProjects\\grade_calculator"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.38.6-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\HP\\AndroidStudioProjects\\grade_calculator\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\HP\\AndroidStudioProjects\\grade_calculator"
+  "FLUTTER_TARGET=C:\\Users\\HP\\AndroidStudioProjects\\grade_calculator\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzguNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OGI4NzI4Njg0OQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzhmYzMwMTJlNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC43"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\HP\\AndroidStudioProjects\\grade_calculator\\.dart_tool\\package_config.json"
+)
